6afcf87973eb9ea45c8cb8270fbc0025aed7fb58
dlsjrdl <133949437+dlsjrdl@users.noreply.github.com>
2023-06-12 21:57:24 +0900

    Add files via upload

--- A.java
+++ A.java
@@ -1,38 +1,37 @@
 package week2;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class A {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("문자를 입력해주세요: ");
-        
-        String grade = scanner.nextLine();
-        String message = "";
 
-        switch (grade) {
-            case "A":
-                message = "Excellent";
-                break;
-            case "B":
-                message = "Good";
-                break;
-            case "C":
-                message = "Usually";
-                break;
-            case "D":
-                message = "Effort";
-                break;
-            case "F":
-                message = "Failure";
-                break;
-            default:
-                message = "error";
-                break;
+        System.out.print("숫자를 입력해줘세요 : ");
+        int month = scanner.nextInt();
+
+        int days;
+
+        if (month >= 1 && month <= 12) {
+            switch (month) {
+                case 2:
+                    days = 28;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    days = 30;
+                    break;
+                default:
+                    days = 31;
+                    break;
+            }
+
+            System.out.println(+ days);
+        } else {
+            System.out.println("Invalid month entered. Please enter a number between 1 and 12.");
         }
-
-        System.out.println(message);
     }
 }
+
 
