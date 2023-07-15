5e50118534d386647d09cf72e47b84b177c6c61c
dlsjrdl <133949437+dlsjrdl@users.noreply.github.com>
2023-07-15 14:20:23 +0900

    Add files via upload

---
+++ 백준_바구니_뒤집기.java
@@ -0,0 +1,35 @@
+package week2;
+import java.util.Scanner;
+
+public class 백준_바구니_뒤집기 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		int basket = sc.nextInt();
+		int M = sc.nextInt();
+
+		int[] basketArray = new int[basket];
+		for(int i = 0; i < basket; i++){
+			basketArray[i] = i+1;
+		}
+
+		for(int i = 0; i < M; i++){
+			int from = sc.nextInt()-1;
+			int to = sc.nextInt()-1;
+
+			while(from < to){
+				int temp = basketArray[from];
+				basketArray[from] = basketArray[to];
+				basketArray[to] = temp;
+				from++;
+				to--;
+			}
+		}
+
+		for(int i = 0; i < basket; i++){
+			System.out.print(basketArray[i]+ " ");
+		}
+	}
+}
+
+
