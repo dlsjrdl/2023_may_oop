7afada57b6d21976a24791ec93311f9aebea0a33
dlsjrdl <133949437+dlsjrdl@users.noreply.github.com>
2023-07-18 19:39:50 +0900

    Add files via upload

---
+++ 백준_단어길이재기.java
@@ -0,0 +1,11 @@
+package week2;
+import java.util.*;
+public class 백준_단어길이재기 {
+public static void main (String args []) {
+	Scanner sc = new Scanner(System.in);
+	
+	String a = sc.next(); //문자열을 입력을 받을것이기 때문에 
+	
+	System.out.println(a.length()); //출력을 할때 입력받는 변수인 a와 length메소드를 이용해서 문자열의 길이를 출력한다 
+}
+}
