29a12e70bd05dacca46a734d4fd839c2cf78704b
dlsjrdl <133949437+dlsjrdl@users.noreply.github.com>
2023-07-22 23:49:49 +0900

    Add files via upload

---
+++ 백준_그대로출력.java
@@ -0,0 +1,16 @@
+package week2;
+import java.util.*;
+public class 백준_그대로출력 {
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNextLine()) {
+			String str = sc.nextLine();
+			System.out.println(str);
+		}
+		sc.close();
+
+	}	
+
+}
