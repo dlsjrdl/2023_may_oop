5ca38be808bb9bd32d1d5860710b81cd1d989a1b
dlsjrdl <133949437+dlsjrdl@users.noreply.github.com>
2023-07-22 23:29:58 +0900

    Add files via upload

---
+++ 백준_단어의개수.java
@@ -0,0 +1,18 @@
+package week2;
+import java.util.*;
+public class 백준_단어의개수 {
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		String S = sc.nextLine(); // 한 라인 입력받기
+		S = S.trim(); // 양쪽 공백제거
+		String ans[] = S.split(" "); // 공백기준으로 스플릿
+		if(ans.length == 1 && ans[0].equals("")) {
+			System.out.println(0);
+		}else {
+			System.out.println(ans.length);
+		}
+	}
+}
+
